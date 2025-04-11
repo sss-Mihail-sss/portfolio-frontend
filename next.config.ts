6668088f18efce604e2dbd66b627ad4d144c734f
mihail <mihai.secrii@gmail.com>
2025-04-11 17:11:34 +0300

    mini changes

--- next.config.ts
+++ next.config.ts
@@ -32,24 +32,24 @@ const nextConfig: NextConfig = {
     },
   },
   env: {
-    DOMAIN: process.env.DOMAIN,
-    API_URL: process.env.API_URL,
-    WEB_URL: process.env.WEB_URL,
-    SESSION_SECRET: process.env.SESSION_SECRET,
+    DOMAIN: process.env.DOMAIN!,
+    API_URL: process.env.API_URL!,
+    WEB_URL: process.env.WEB_URL!,
+    SESSION_SECRET: process.env.SESSION_SECRET!,
 
     // AWS S3
-    AWS_REGION: process.env.AWS_REGION,
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION!,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
 
     // ChatGPT KEY
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY!,
 
     // Google KEY
-    GOOGLE_PLACE_API_KEY: process.env.GOOGLE_PLACE_API_KEY,
+    GOOGLE_PLACE_API_KEY: process.env.GOOGLE_PLACE_API_KEY!,
 
     // Blob KEY
-    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN!,
   },
   images: {
     // GitHub Images
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,12 +4,23 @@ import { notFound } from 'next/navigation';
 
 import { BaseLayout } from '@/components/layouts/base';
 import { locales } from '@/i18n/routing';
+import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 
 type Props = {
   children: ReactNode;
   params: Promise<{
     locale: Locale;
   }>;
+}
+
+export async function generateMetadata({ params }: Omit<Props, 'children'>): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'meta' });
+
+  return {
+    metadataBase: new URL(process.env.API_URL!),
+  };
 }
 
 export function generateStaticParams() {
